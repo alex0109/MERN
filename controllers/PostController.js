7b8559771e22e26b5d0b7f9b8d6073623edf0152
alex0109 <krossyouyub@gmail.com>
2022-10-04 12:12:22 +0200

    fast commit

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,52 @@
 import PostModel from '../models/Post.js'
 
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec();
+        res.json(posts)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: 'Couldnt get all posts... :/'
+        })
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postID = req.params.id;
+        PostModel.findOneAndUpdate({
+            _id: postID
+        }, {
+            $inc: { viewsCount: 1 }
+        },
+        {
+            returnDocument: 'after'
+        },
+        (error, doc) => {
+            if(error) {
+                console.log(error)
+                return res.status(500).json({
+                    message: 'Couldnt get post... :/'
+                })
+            }
+
+            if(!doc) {
+                return res.status(404).json({
+                    message: 'Post didnt find'
+                })
+            }
+
+            res.json(doc)
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: 'Couldnt get all posts... :/'
+        })
+    }
+}
+
 export const create = async (req, res) => {
     try {
         const doc = new PostModel({
